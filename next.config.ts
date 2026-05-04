d7f1c5dd9a7d38b77e30edb946406256bef02f05
shhhll <afhxzz01@gmail.com>
2026-05-04 16:25:55 +0900

    Add Next.js configuration for export output

--- next.config.ts
+++ next.config.ts
@@ -1,1 +1,7 @@
-
+import type { NextConfig } from 'next';
+                                                                                     
+  const nextConfig: NextConfig = {
+    output: 'export',                                                                
+  };                                                        
+    
+  export default nextConfig;
